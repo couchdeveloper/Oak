48ef1f0836cbf547a8fa7ab2df0ea8e6cf25cb10
Andreas Grosam <couchdeveloper@gmail.com>
2025-06-19 13:25:00 +0200

    Remove Sendable from Callback
    This commit removes the Sendable conformance from `Callback`, but
    it is still implied when the associated type `Value` conforms to
    Sendable.

--- Sources/Oak/FSM/Callbacks.swift
+++ Sources/Oak/FSM/Callbacks.swift
@@ -1,13 +1,13 @@
 /// A `Callback` value encapsulates an isolated closure which takes a parameter of
 /// type `value` as an argument.
-public struct Callback<Value: Sendable>: Subject, Sendable {
-    let fn: @Sendable @isolated(any) (Value) throws -> Void
+public struct Callback<Value>: Subject {
+    let fn: @Sendable @isolated(any) (sending Value) throws -> Void
     
     /// Initialises a `Callback` value with the given isolated throwing closure.
     ///
     /// - Parameter fn: A throwing closure which will be called when `Self`
     /// receives a value via its `send(_:)` function.
-    public init(_ fn: @escaping @Sendable @isolated(any) (Value) throws -> Void) {
+    public init(_ fn: @escaping @Sendable @isolated(any) (sending Value) throws -> Void) {
         self.fn = fn
     }
 
@@ -18,14 +18,16 @@ public struct Callback<Value: Sendable>: Subject, Sendable {
     
     /// Send a value to `Self` which calls its callback clouser with the argument `value`.
     /// - Parameter value: The value which is used as the argument to the callback closure.
-    public func send(_ value: Value) async throws {
+    public func send(_ value: sending Value) async throws {
         try await fn(value)
     }
 }
+
+extension Callback: Sendable where Value: Sendable {}
 
 /// A convenient struct which defines a `Callback` value whose closure is a no-op.
 public struct NoCallbacks<Value>: Subject {
     public init() {}
     /// Sending the value has no effect.
-    public func send(_ value: Value) throws {}
+    public func send(_ value: sending Value) throws {}
 }
